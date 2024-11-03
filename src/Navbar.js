91a20d590575f4eed3fb53c9a59d3e7621f939b2
fjrmdhna <mfajarramadhana@gmail.com>
2024-11-03 22:45:36 +0700

    menggeser logo navbar ke kiri

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,30 +1,28 @@
 // src/components/Navbar.js
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom'; // Import Link dari react-router-dom
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import logo from './Indosat_Ooredoo_Hutchison.png'; 
 import './Navbar.css';
 
 const MyNavbar = () => {
   return (
     <Navbar expand="lg" className="custom-navbar">
-      <Container>
-        <Navbar.Brand as={Link} to="/" className="navbar-brand-custom">
-          <img
-            src={logo}
-            alt="Indosat Logo"
-            className="navbar-logo"
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="navbar-links">
-            <Nav.Link as={Link} to="/">Dashboard</Nav.Link>
-            <Nav.Link as={Link} to="/notification">Notification</Nav.Link>
-            <Nav.Link as={Link} to="/planning">Planning</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+      <Navbar.Brand as={Link} to="/" className="navbar-brand-custom">
+        <img
+          src={logo}
+          alt="Indosat Logo"
+          className="navbar-logo"
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="navbar-links">
+          <Nav.Link as={Link} to="/">Dashboard</Nav.Link>
+          <Nav.Link as={Link} to="/notification">Notification</Nav.Link>
+          <Nav.Link as={Link} to="/planning">Planning</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
